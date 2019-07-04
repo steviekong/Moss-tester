b7a877aa59d0aa245114ae7a4fba9e375ae821de
steviekong <sidharth.reji@gmail.com>
2019-07-04 22:57:27 +0530

    fixed html files and input parsing

--- moss.py
+++ moss.py
@@ -2,7 +2,12 @@ import mosspy
 import sys
 
 def get_moss_results(userid, basefile_name, testfiles_path ,language, report_path):
-	moss_obj = mosspy.Moss(userid, language)
+	real_language = None
+	if language == 'html': 
+		real_language = 'javascript'
+	else: 
+		real_language = language
+	moss_obj = mosspy.Moss(userid, real_language)
 
 	moss_obj.addBaseFile(basefile_name)
 
@@ -11,7 +16,8 @@ def get_moss_results(userid, basefile_name, testfiles_path ,language, report_pat
 		"python" : "py",
 		"c++" : "cpp",
 		"c" : "c",
-		"java" : "java"
+		"java" : "java",
+		'html' : 'html'
 	}
 
 	moss_obj.addFilesByWildcard(testfiles_path+"/*."+language_extensions[language])
@@ -23,14 +29,13 @@ def get_moss_results(userid, basefile_name, testfiles_path ,language, report_pat
 
 
 def main():
-	if len(sys.argv) <  5:
-		if sys.argv[1] is '--help':
-			print("To run a moss test use:\n\'python moss.py <userid> <language> <basefile name> <testfiles directory path> <report directory>\'\nTestfiles must all be in the same directory!\nReport is generated in the same directory as the script!")
+	if len(sys.argv) <  6:
+		if len(sys.argv) == 1 and sys.argv[1] == '--help':
+			print("To run a moss test use:\n\'python moss.py <userid> <language> <basefile name> <testfiles directory path> <report directory>\'\nTestfiles must all be in the same directory!\nReport is generated in a directory called \'report\'\n")
 		else:
-			print('Too few arguments!\nFormat is \'python moss.py <userid> <language> <basefile name> <testfiles directory path> <report directory>\'\nType \'python moss.py --help\' to get help.>\'')
-		sys.exit(1)
+			print('Too few arguments!\nFormat is \'python moss.py <userid> <language> <basefile name> <testfiles directory path> <report directory>\'\nType \'python moss.py --help\' to get help\' or read README.md')	
+		sys.exit(0)
 	else:
-		print(len(sys.argv))
 		get_moss_results(userid = sys.argv[1], language = sys.argv[2], basefile_name = sys.argv[3], testfiles_path = sys.argv[4], report_path = sys.argv[5])
 
 if __name__ == '__main__':
